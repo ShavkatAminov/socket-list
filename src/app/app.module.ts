4c61b7ee6bb947c47c70750976377548a8e26405
shavkat.aminov <sh.aminov@ssdsoftwaresolutions.com>
2023-08-02 15:52:15 +0500

    add tailwind and form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InputComponent } from './components/input/input.component';
 import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponent
   ],
-    imports: [
-        BrowserModule,
-        MatInputModule
-    ],
+  imports: [
+    BrowserModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
