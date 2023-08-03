e806444e5413987c92557fb6c320c323b5053a8a
shavkat.aminov <sh.aminov@ssdsoftwaresolutions.com>
2023-08-03 14:18:57 +0500

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {AppModule} from "./app.module";
+import {InputComponent} from "./components/input/input.component";
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -12,11 +15,31 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should render correctly', () => {
+    const nativeElement = fixture.nativeElement;
+    expect(nativeElement.querySelectorAll('app-input').length).toEqual(2);
+    expect(nativeElement.querySelectorAll('app-array-input').length).toEqual(1);
+    expect(nativeElement.querySelectorAll('table').length).toEqual(1);
+  });
+
+  it('should have been call setWorker', () => {
+    spyOn(component, 'setWorker');
+    component.ngOnInit();
+    expect(component.setWorker).toHaveBeenCalledWith(component.parameters.value);
+  });
+
+  it('should have been call setExtraIDs', () => {
+    spyOn(component, 'setExtraIDs');
+    component.ngOnInit();
+    expect(component.setExtraIDs).toHaveBeenCalledWith(component.extraIds.value);
   });
 });
